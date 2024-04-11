0821b8998299f22f15eaeb02839987e9cb5fc5fe
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-11 17:56:43 +0900

    KBB-20 [Refactore] FrontUrlGenerator 파라미터 간결화

--- src/main/java/org/bootstrap/post/utils/FrontUrlGenerator.java
+++ src/main/java/org/bootstrap/post/utils/FrontUrlGenerator.java
@@ -1,10 +1,11 @@
 package org.bootstrap.post.utils;
 
-import org.bootstrap.post.entity.CategoryType;
+import org.bootstrap.post.entity.Post;
 
 public class FrontUrlGenerator {
     private final static String FRONTEND_BASE_URL = "http://localhost:5173/";
-    public static String createFrontUrl (Long postId, String moldevId, CategoryType categoryType) {
-        return FRONTEND_BASE_URL + moldevId + "/" + categoryType + "/" + postId;
+
+    public static String createFrontUrl(Post post, String moldevId) {
+        return FRONTEND_BASE_URL + moldevId + "/" + post.getCategory() + "/" + post.getId();
     }
 }
