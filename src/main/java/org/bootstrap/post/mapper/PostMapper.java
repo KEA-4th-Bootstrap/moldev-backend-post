18f2b92f28d3b5200908c9af8af5c8ff9d9dd10c
Ginieee <kaj1226@naver.com>
2024-04-11 16:00:35 +0900

    KBB-18 [Feat] PostMapper 구현

--- src/main/java/org/bootstrap/post/mapper/PostMapper.java
+++ src/main/java/org/bootstrap/post/mapper/PostMapper.java
@@ -1,4 +1,12 @@
 package org.bootstrap.post.mapper;
 
+import org.bootstrap.post.dto.request.PostRequestDto;
+import org.bootstrap.post.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PostMapper {
+    public Post toEntity (PostRequestDto requestDto, Long memberId, String thumbnail) {
+        return Post.createPost(requestDto, memberId, thumbnail);
+    }
 }
