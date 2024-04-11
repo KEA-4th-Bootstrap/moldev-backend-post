668c865c8cadbbe7742677f169ae214b71f9ab72
Ginieee <kaj1226@naver.com>
2024-04-11 16:00:00 +0900

    KBB-18 [Feat] PostController 구현

--- src/main/java/org/bootstrap/post/controller/PostController.java
+++ src/main/java/org/bootstrap/post/controller/PostController.java
@@ -1,6 +1,12 @@
 package org.bootstrap.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.bootstrap.post.common.SuccessResponse;
+import org.bootstrap.post.dto.request.PostRequestDto;
+import org.bootstrap.post.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/post")
 @RestController
 public class PostController {
-
+    private final PostService postService;
+    @PostMapping
+    public ResponseEntity<SuccessResponse<?>> createPost(final Long memberId,
+                                                         @RequestBody final PostRequestDto requestDto) {
+        postService.createPost(memberId, requestDto);
+        return SuccessResponse.created(null);
+    }
 }
