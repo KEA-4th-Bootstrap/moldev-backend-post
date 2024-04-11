6e7d848bc7d1b1f61046dafa237ee408abb48a6f
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-11 17:20:45 +0900

    KBB-20 [Feat] S3 예외 코드 추가

--- src/main/java/org/bootstrap/post/common/error/GlobalErrorCode.java
+++ src/main/java/org/bootstrap/post/common/error/GlobalErrorCode.java
@@ -14,8 +14,12 @@ public enum GlobalErrorCode implements BaseErrorCode {
     HTTP_MESSAGE_NOT_READABLE(BAD_REQUEST,"GLOBAL_400_1", "잘못된 형식의 값을 입력했습니다."),
     _INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "GLOBAL_500_1", "서버 오류. 관리자에게 문의 부탁드립니다."),
 
-    /*Enum error*/
-    INVALID_ENUM_CODE(BAD_REQUEST, "ENUM_400_1", "잘못된 Enum class code 입니다.")
+    /* Enum error */
+    INVALID_ENUM_CODE(BAD_REQUEST, "ENUM_400_1", "잘못된 Enum class code 입니다."),
+
+    /* S3 */
+    INVALID_FILE_UPLOAD(BAD_REQUEST, "S3_400_1", "파일을 업로드할 수 없습니다."),
+    INVALID_MULTIPART_FILE(BAD_REQUEST, "S3_400_2", "잘못된 Multipart 입니다."),
     ;
 
     private HttpStatus status;
