ff6e3879bec9534d165b40190b685af64a7695e1
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-11 09:51:19 +0900

    KBB-15 [Feat] post query dsl repository impl 추가

---
+++ src/main/java/org/bootstrap/post/repository/PostQueryRepositoryImpl.java
@@ -0,0 +1,9 @@
+package org.bootstrap.post.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class PostQueryRepositoryImpl implements PostQueryRepository{
+}
