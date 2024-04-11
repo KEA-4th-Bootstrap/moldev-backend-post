ac34a5365795ba7b4d6ce784b097b49e0634ef05
Ginieee <kaj1226@naver.com>
2024-04-11 16:28:16 +0900

    KBB-18 [Fix] CategoryType 자기소개 추가

--- src/main/java/org/bootstrap/post/entity/CategoryType.java
+++ src/main/java/org/bootstrap/post/entity/CategoryType.java
@@ -11,7 +11,8 @@ public enum CategoryType implements EnumField {
     ACTIVITY(1, "대외활동"),
     PROJECT(2, "프로젝트"),
     AWARDS(3, "수상이력"),
-    TROUBLE(4, "트러블슈팅");
+    TROUBLE(4, "트러블슈팅"),
+    INTRODUCE(5, "자기소개");
 
     private Integer code;
     private String desc;
