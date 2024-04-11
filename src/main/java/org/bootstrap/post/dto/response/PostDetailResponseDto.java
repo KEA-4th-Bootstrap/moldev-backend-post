e393929973fe4287e297b8d0c825602e207b6241
Ginieee <kaj1226@naver.com>
2024-04-11 22:28:37 +0900

    KBB-24 [Feat] Post 조회 시의 PostDetailResponseDto 구현

---
+++ src/main/java/org/bootstrap/post/dto/response/PostDetailResponseDto.java
@@ -0,0 +1,26 @@
+package org.bootstrap.post.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.bootstrap.post.entity.Post;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record PostDetailResponseDto(
+        Long id,
+        Long memberId,
+        String title,
+        String content,
+        String thumbnail,
+        String category
+) {
+    public static PostDetailResponseDto of (Post post, String category) {
+        return PostDetailResponseDto.builder()
+                .id(post.getId())
+                .memberId(post.getMemberId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .thumbnail(post.getThumbnail())
+                .category(category)
+                .build();
+    }
+}
