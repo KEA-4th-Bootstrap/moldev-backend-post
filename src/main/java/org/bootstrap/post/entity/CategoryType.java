d0764eb8e312b932c0a1ef402e6c7a63d776d331
Ginieee <kaj1226@naver.com>
2024-04-11 15:57:33 +0900

    KBB-18 [Feat] CategoryType 구현

---
+++ src/main/java/org/bootstrap/post/entity/CategoryType.java
@@ -0,0 +1,18 @@
+package org.bootstrap.post.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.bootstrap.post.utils.EnumField;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public enum CategoryType implements EnumField {
+    ACTIVITY(1, "대외활동"),
+    PROJECT(2, "프로젝트"),
+    AWARDS(3, "수상이력"),
+    TROUBLE(4, "트러블슈팅");
+
+    private Integer code;
+    private String desc;
+}
