1e724c3a992af155589a411ba23217586aace7de
RyuKwanKon <rhks33rhs@gmail.com>
2024-04-11 18:19:12 +0900

    KBB-20 [Feat] s3 환경 추가

--- src/main/java/org/bootstrap/post/helper/PostHelper.java
+++ src/main/java/org/bootstrap/post/helper/PostHelper.java
@@ -3,12 +3,20 @@ package org.bootstrap.post.helper;
 import lombok.RequiredArgsConstructor;
 import org.bootstrap.post.entity.Post;
 import org.bootstrap.post.repository.PostRepository;
+import org.bootstrap.post.utils.S3Provider;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Component
 public class PostHelper {
+    private final static String IMAGE_TYPE = "thumbnail";
     private final PostRepository postRepository;
+    private final S3Provider s3Provider;
+
+    public String createStringThumbnail(MultipartFile multipartFile) {
+        return s3Provider.uploadFile(multipartFile, IMAGE_TYPE);
+    }
 
     public Post savePost (Post post) {
         return postRepository.save(post);
