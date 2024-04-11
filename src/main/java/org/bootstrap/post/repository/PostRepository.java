ea377429f9c8bd5cb4b36cb468be8a5cba353934
Ginieee <kaj1226@naver.com>
2024-04-11 16:00:45 +0900

    KBB-18 [Feat] PostRepository 구현

--- src/main/java/org/bootstrap/post/repository/PostRepository.java
+++ src/main/java/org/bootstrap/post/repository/PostRepository.java
@@ -1,4 +1,8 @@
 package org.bootstrap.post.repository;
 
-public interface PostRepository {
+import org.bootstrap.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
+
 }
