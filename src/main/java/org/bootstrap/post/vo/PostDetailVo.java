c24b643d522f727be31994276c5ffbf53573bbdf
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-11 01:28:12 +0900

    KBB-68 [Feat] 전체 목록 조회 vo 구현

---
+++ src/main/java/org/bootstrap/post/vo/PostDetailVo.java
@@ -0,0 +1,14 @@
+package org.bootstrap.post.vo;
+
+import lombok.Builder;
+import org.bootstrap.post.entity.CategoryType;
+
+@Builder
+public record PostDetailVo(
+        Long id,
+        String title,
+        String content,
+        String thumbnail,
+        CategoryType category
+) {
+}
