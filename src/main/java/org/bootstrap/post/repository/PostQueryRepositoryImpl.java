2ff1e90d31d3c0be5054f4742bcebbb3351011ab
RyuKwanKon <rhks33rhs@gmail.com>
2024-05-11 01:28:45 +0900

    KBB-68 [Feat] 게시글 카테고리 조회 & 전체 목록 조회 api 쿼리 구현

--- src/main/java/org/bootstrap/post/repository/PostQueryRepositoryImpl.java
+++ src/main/java/org/bootstrap/post/repository/PostQueryRepositoryImpl.java
@@ -1,9 +1,85 @@
 package org.bootstrap.post.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.bootstrap.post.entity.CategoryType;
+import org.bootstrap.post.entity.Post;
+import org.bootstrap.post.vo.PostCategoryInfoVo;
+import org.bootstrap.post.vo.PostDetailVo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+
+
+import java.util.List;
+
+import static org.bootstrap.post.entity.QPost.post;
 
 @RequiredArgsConstructor
-@Repository
-public class PostQueryRepositoryImpl implements PostQueryRepository{
+public class PostQueryRepositoryImpl implements PostQueryRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<PostDetailVo> findPostDetailVos(Long memberId, Pageable pageable) {
+        List<PostDetailVo> contents = jpaQueryFactory
+                .select(Projections.constructor(PostDetailVo.class,
+                        post.id,
+                        post.title,
+                        post.content,
+                        post.thumbnail,
+                        post.category
+                ))
+                .from(post)
+                .where(
+                        eqMemberId(memberId)
+                )
+                .orderBy(post.id.desc())
+                .fetch();
+
+        JPAQuery<Post> countQuery = jpaQueryFactory
+                .selectFrom(post)
+                .where(
+                        eqMemberId(memberId)
+                );
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchCount);
+    }
+
+    @Override
+    public Page<PostCategoryInfoVo> findPostCategoryInfoVos(Long memberId, CategoryType type, Pageable pageable) {
+        List<PostCategoryInfoVo> contents = jpaQueryFactory
+                .select(Projections.constructor(PostCategoryInfoVo.class,
+                        post.id,
+                        post.title,
+                        post.content,
+                        post.thumbnail
+                ))
+                .from(post)
+                .where(
+                        eqMemberId(memberId),
+                        eqCategoryType(type)
+                )
+                .orderBy(post.id.desc())
+                .fetch();
+
+        JPAQuery<Post> countQuery = jpaQueryFactory
+                .selectFrom(post)
+                .where(
+                        eqMemberId(memberId),
+                        eqCategoryType(type)
+                );
+
+        return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchCount);
+    }
+
+    private BooleanExpression eqMemberId(Long memberId) {
+        return memberId == null ? null : post.memberId.eq(memberId);
+    }
+
+    private BooleanExpression eqCategoryType(CategoryType type) {
+        return type == null ? null : post.category.eq(type);
+    }
 }
