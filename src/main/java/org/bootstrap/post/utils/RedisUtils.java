4b94734746a740d92efa2d71e6eb9dff215b0129
kdhhuns2000 <52370750+kdhhuns2000@users.noreply.github.com>
2024-05-15 19:54:09 +0900

    KBB-98 [Feat] zset operation 추가

--- src/main/java/org/bootstrap/post/utils/RedisUtils.java
+++ src/main/java/org/bootstrap/post/utils/RedisUtils.java
@@ -3,6 +3,7 @@ package org.bootstrap.post.utils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
@@ -19,5 +20,9 @@ public class RedisUtils {
 
     public ValueOperations<String, String> getValueOperations() {
         return redisTemplate.opsForValue();
+    }
+
+    public ZSetOperations<String, String> getZSetOperations() {
+        return redisTemplate.opsForZSet();
     }
 }
