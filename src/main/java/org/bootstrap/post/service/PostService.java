c7a127793aa4ec388af510800d845c009ebd785a
RyuKwanKon <rhks33rhs@gmail.com>
2024-06-07 18:05:37 +0900

    [fix] post Image 수정 로직 반영

--- src/main/java/org/bootstrap/post/service/PostService.java
+++ src/main/java/org/bootstrap/post/service/PostService.java
@@ -96,7 +96,7 @@ public class PostService {
     public void updatePost(Long postId, PostRequestDto requestDto) {
         Post post = postHelper.findPostOrThrow(postId);
         postHelper.deletePostImages(post.getId());
-        PostImage postImage = postHelper.findPostImageOrThrow(post.getId());
+        PostImage postImage = createPostImageAndSave(post, requestDto);
         post.updatePost(requestDto);
         postImage.updateImages(requestDto.images());
     }
